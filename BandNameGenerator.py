42757e9bbd17c95550fd920c23c4920cd69ba8a0
b3tcee <betsyotoyo@gmail.com>
2023-01-02 12:35:31 +0100

    Day One

---
+++ BandNameGenerator.py
@@ -0,0 +1,19 @@
+#Create a greeting
+#Get name of City
+#Get name of Pet
+#Concatenate
+
+#Welcome Greeting
+print("Welcome to your Band Name Generator!")
+
+#Get user input
+name_of_city = input("What's the name of the city you grew up in? \n")
+name_of_pet = input("What's the name of your pet? \n")
+
+#Concatenate the names
+
+Band_name = "Your Band Name could be " + name_of_city +" " +  name_of_pet
+print(Band_name)
+
+
+
---
+++ Ex 1.4.py
@@ -0,0 +1,22 @@
+#  Don't change the code below 
+a = input("a: ")
+b = input("b: ")
+
+
+####################################
+#Write your code below this line
+# Assign a1 to carry the variable for a, for instance 3, so a1 becomes 3 
+a1 = a
+# Assign b1 to carry the variable for b, for instance 5, so b1 becomes 5
+a= b
+#Then assign a to carry the variable of b1, so a becomes 5
+b = a1
+#Then assign b to carry the variable of a1, so a becomes 3
+
+
+#Write your code above this line
+####################################
+
+#  Don't change the code below 
+print("a: " + a)
+print("b: " + b)
